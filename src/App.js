5638c76524b6a8b1a7521c5251e8f6cbba660ac2
samuel vidal <111181991+vidalYep@users.noreply.github.com>
2024-11-20 00:00:57 +0000

    Feat: Adiciona Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./styles.css";
+import { Header } from "./Header";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Header />;
 }
